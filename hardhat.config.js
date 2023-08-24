222c685330dc86257cfb207f19e67d4edf407acc
raphaelzhou1 <tianyu_zhou1@brown.edu>
2023-08-24 01:04:49 -0700

    compile dss successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.8.4", // Use your desired version
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+      },
+    ],
+  },
   paths: {
-    sources: "./src",  // Source directory for your .sol files
-    tests: "./src/test", // Directory for your test files
+    sources: "./src", 
+    tests: "./src/test",
     cache: "./cache",
     artifacts: "./artifacts"
   }
